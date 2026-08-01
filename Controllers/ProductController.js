0cc764e270f68f316ad7f0737bf26f38cf8b5a39
Dada Olakunle <dadaolakunle43@gmail.com>
2026-08-01 03:14:38 +0100

    Login completed

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -45,3 +45,44 @@ exports.updateProduct = async (req, res) => {
         res.status(400).json({ message: 'Error updating product', error: error.message });
     }
 };
+
+//get product by id
+exports.getProductById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await Product.findById(id);
+
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+        res.json({ message: 'Product found', product });
+    } catch (error) {
+        res.status(400).json({ message: 'Error fetching product', error: error.message });
+    }
+};
+
+//get all products
+exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json({ message: 'Products fetched successfully', products });
+    } catch (error) {
+        res.status(400).json({ message: 'Error fetching products', error: error.message });
+    }
+};
+
+//delete product by id
+exports.deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        } 
+        res.json({ message: 'Product deleted successfully', product });
+    } catch (error) {
+        res.status(400).json({ message: 'Error deleting product', error: error.message });
+    }
+};
--- Routes/ProductRoute.js
+++ Routes/ProductRoute.js
@@ -7,8 +7,10 @@ const productController = require('../controllers/ProductController');
 
 //define the routes
 router.post('/createproduct', productController.createProduct);
-
 router.put('/updateproduct/:id', productController.updateProduct);
+router.get('/getproduct/:id', productController.getProductById);
+router.get('/getallproducts', productController.getAllProducts);
+router.delete('/deleteproduct/:id', productController.deleteProduct);
 
 //export the router
 module.exports = router;
